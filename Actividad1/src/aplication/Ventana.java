bd171b93ba31d2d685bdc417cd01d80db91fa999
angelMV31 <angelgabr2_23@alu.uabcs.mx>
2025-02-13 18:41:46 -0700

    Commit

--- Actividad1/src/aplication/Ventana.java
+++ Actividad1/src/aplication/Ventana.java
@@ -1,28 +1,61 @@
 package aplication;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame {
+	Font etiquetas = new Font("Andale mono", Font.BOLD,16);
 	
 	public Ventana(String title) {
 		this.setTitle(title);
 		this.setVisible(true);
-		this.setSize(500,500);
-		this.setTitle("Login");
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		this.setSize(500,500);
+		
+		this.setResizable(false);
+
 		this.setLocationRelativeTo(null);
 		
-		JLabel etiqueta1 = new JLabel("Bienvenido");
-		etiqueta1.setSize(100,100);
-		etiqueta1.setLocation(10, 50);
-		etiqueta1.setBackground(Color.ORANGE);
+		this.setMinimumSize(new Dimension(350,350));
+		this.setMaximumSize(new Dimension(750,750));
+		this.add(this.login());
+		this.repaint();
 		
-		this.add(etiqueta1);
+	}
+	
+	public JPanel login() {
+		JPanel login = new JPanel();
+		login.setLocation(0, 0);
+		login.setSize(500, 500);
+		login.setOpaque(true);
+		login.setBackground(new Color(229,114,126));
+		login.setVisible(true);
+		login.setLayout(null);
+		
+		JLabel etiqueta1 = new JLabel("Bienvenidos");
+		etiqueta1.setSize(160,40);
+		etiqueta1.setLocation(160, 10);
+		etiqueta1.setBackground(Color.gray);
+		etiqueta1.setOpaque(true);
+		etiqueta1.setFont(etiquetas);
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER);
+		login.add(etiqueta1);
+		
+		
+		
+		login.revalidate();
+		
+		return login;
+
 	}
 
 }
