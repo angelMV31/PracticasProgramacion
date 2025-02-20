f8758dad1ff78612b552ec4f193254a91f6bf12b
angelMV31 <angelgabr2_23@alu.uabcs.mx>
2025-02-19 22:48:04 -0700

    Unidad 2 Ejercicio 16

--- Actividad1/src/aplication/Main.java
+++ Actividad1/src/aplication/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Ventana ventana = new Ventana("Acceso");
+		Ventana ventana = new Ventana("Calculadora");
 
 	}
 
